b3b9efda97b170c86c7df752bae4c44f6c4feee3
Ryan Duckworth <ryan.duckworth.971@my.csun.edu>
2016-05-05 18:10:03 -0700

    Updated file structure and added recovery policy

--- Project/Database/Disaster Recovery/database_backup.sql
+++ Project/Database/Disaster Recovery/database_backup.sql
@@ -2,12 +2,11 @@
 --Purpose: The purpose of this script is to provide a simple way to backup the s16guest06 database so that in the
 --         event of a disaster, the database can be recovered intact. Backups should be performed at least weekly,
 --		   if not more, to ensure that key data coming into this low volume database is not at risk of being lost.
---Issues:  This script uses the servers internal filesystem so it won't work. However, it's purpose is to illustrate
+--Issues:  This script uses the server's internal filesystem so it won't work. However, it's purpose is to illustrate
 --         how it could be used in a disaster scenario.
 
 DECLARE @filename VARCHAR(20)
 DECLARE @filepath VARCHAR(100)
-DECLARE @full_filename VARCHAR(100)
 DECLARE @backup_date VARCHAR(10)
 
 SET @filepath = 'C:\SQL_Backups\'
